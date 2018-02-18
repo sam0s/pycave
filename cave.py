829b58fd435a6f5bdcae6b8442b5192318994333
Sam Tubb <samiscoolco@gmail.com>
2018-02-18 02:08:23 -0500

    honestly just testing git

--- cave.py
+++ cave.py
@@ -17,12 +17,20 @@ class genCollide(Entity):
         Entity.__init__(self)
         self.image = Surface((64,64))
         self.rect = Rect(x,y,64,64)
+        ################ [x,y]
+        self.direction = [1,0]
+        
+    def Update(self):
+        self.rect.x+=self.direction[0]
+        self.rect.y+=self.direction[1]
 
 class caveBlock(Entity):
     def __init__(self,x,y):
         Entity.__init__(self)
         self.image = Surface((16,16));self.image.fill((100,100,100))
         self.rect = Rect(x,y,16,16)
+        
+
 
 blocklist=pygame.sprite.Group()
 
@@ -33,7 +41,10 @@ for a in range(GAME_HEIGHT/16):
     for f in range(GAME_WIDTH/16):
         blocklist.add(caveBlock(f*16,a*16))
 
-pygame.sprite.spritecollide(carve, blocklist, True)
+
+for a in range(322):
+    carve.Update()
+    pygame.sprite.spritecollide(carve, blocklist, True)
 
 blocklist.draw(screen)
 
@@ -42,3 +53,5 @@ window.blit(screen,(0,0))
 pygame.display.flip()
 
 sleep(3)
+
+pygame.display.quit()
